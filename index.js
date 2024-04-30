f12e4eb9fcb9679a37125a5c61ee4b48e0a43772
Arsh2609 <158245235+Arsh2609@users.noreply.github.com>
2024-04-30 20:18:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5000;
+const { ConnectMongoDB } = require("./connection");
+const { CheckforAuthCookie } = require("./middlewares/auth");
+const router = require("./routes/user");
+const itemRouter = require("./routes/items");
+// const MapRouter = require("./routes/map");
+
+app.listen(PORT, () => console.log(`Server Running on PORT:${PORT}`));
+ConnectMongoDB("mongodb+srv://arsh:arsh@cluster0.e7049kx.mongodb.net/foodie")
+  .then(() => console.log("MongoDB Connected Successfully."))
+  .catch((err) => console.log("Error Connecting MongoDB", err));
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(CheckforAuthCookie("token"));
+app.use(express.json());
+app.use(cors());
+
+// app.use("/api/geocode/",MapRouter)
+app.use("/api/add-new/",itemRouter)
+app.use("/api", router);
+app.get("/test",(req,res)=>{
+  return res.send("testing");
+})
